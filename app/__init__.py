637fc4d5f19bdf4040a2c0323d35f926fcfc5020
Antonio Matos <acxmatos@gmail.com>
2020-08-14 17:45:53 -0300

    Finished Chapter 22

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import rq
 import logging
 
 from flask import Flask, request, current_app
@@ -11,6 +12,7 @@ from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from elasticsearch import Elasticsearch
+from redis import Redis
 
 from config import Config
 
@@ -40,6 +42,8 @@ def create_app(config_class=Config):
     babel.init_app(app)
 
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
